3260feb0b6893e828084af32bf77939639d1baf7
arunkbharathan <arunkbharathan@gmail.com>
2017-01-10 16:28:10 +0530

    animation

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 function first (){
+//    let mysum = document.getElementById("summary");
+//    let myul = mysum.getElementsByTagName("ul")[0];
+//    let newli = document.createElement("li");
+//    newli.innerHTML="Skill "+i;
+//    myul.appendChild(newli);
     let col1s = document.getElementsByClassName("col1");
     col1s =  Array.prototype.slice.apply(col1s);
     for(let tmp of col1s){
@@ -18,7 +23,11 @@ function second(){
 
 
 window.onload = function(){
-
+    
+    //setTimeout(start,1000/2,1); 
+   // setTimeout(start,2000/2,2); 
+    //setTimeout(start,3000/2,3); 
+   // setTimeout(start,4000/2,4); 
     setTimeout(first,200); 
     setTimeout(second,400); 
     
